2980caa4ddf9235ed021856619b3391f61fd7f73
vennhost <samogconsult@gmail.com>
2020-12-12 10:38:50 +0100

    configure cors

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-const express = require('express')
-const app = express()
-const passport = require("passport")
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const passport = require("passport");
+const mongoose = require("mongoose");
 
-const userRouter = require("./src/routes/users")
-
+const userRouter = require("./src/routes/users");
 
 const mongooseConnection = require("./src/db/mongoose");
 require("dotenv").config();
 const port = process.env.PORT || 3300;
 mongooseConnection();
 
-
-
 app.use(express.json());
-app.use(passport.initialize())
+app.use(passport.initialize());
 
+app.use(cors());
 
+app.use("/users", userRouter);
 
-app.use("/users", userRouter)
+app.get("/", (req, res) => res.send("Hello World!"));
 
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
