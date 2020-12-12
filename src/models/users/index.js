926a44fb3e606f2fdf557dd5047ce9bca6434567
vennhost <samogconsult@gmail.com>
2020-12-12 09:41:54 +0100

    resgister

--- src/models/users/index.js
+++ src/models/users/index.js
@@ -1,13 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
- 
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const userSchema = new Schema({
-    username: {
-        type: String,
-    }
+  username: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
 });
- 
+
 userSchema.plugin(passportLocalMongoose);
- 
-module.exports = mongoose.model('User', userSchema);
+
+module.exports = mongoose.model("User", userSchema);
